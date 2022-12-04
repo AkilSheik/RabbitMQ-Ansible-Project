5124f63b2277b47c81d47db3de3b1ffcf4d40a5e
Ubuntu <akil.sheik@gmail.com>
2022-12-04 22:10:18 +0000

    Update

--- send.py
+++ send.py
@@ -3,8 +3,10 @@ import sys
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello',body= f"{sys.argv[1]}")
-print(f"{sys.argv[1]}")
+channel.queue_declare(queue='queue', durable=True)
+channel.basic_publish(exchange='', routing_key='hello',body= f"{sys.argv[1]}", properties=pika.BasicProperties(
+                         delivery_mode = pika.spec.PERSISTENT_DELIVERY_MODE
+                      ))
+print(f"{sys.argv[1]} was sent")
 connection.close()
 
